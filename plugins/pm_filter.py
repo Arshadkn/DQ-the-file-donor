2851511161376d1efe7630e7f522cfcede67c46f
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-03-08 21:56:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -577,7 +577,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     await query.answer(f"⚠️  {query.from_user.first_name}, മറ്റുള്ളവർ റിക്വസ്റ്റ് ചെയ്ത മുവിയിൽ കുത്തി നോക്കാതെ. Broo വേണ്ടത് Broo റിക്വസ്റ്റ് ചെയ്യുക🤒.\n\n❕ Broo, Search Your Own File.Don't Click Others Request File's ❕", show_alert=True)
                 await query.answer('<b>👋 Hᴀɪ{query.from_user.first_name}Files ഞാൻ pmമിൽ അയിചിട്ടുണ്ട്🌿.\n\n👋Hᴀɪ Bʀᴏᴏ Yᴏᴜʀ Fɪʟᴇs Aʀᴇ Sᴇɴᴛᴇᴅ Pm (Pᴇʀsᴏɴᴀʟʟʏ) Cʜᴇᴄʜ Iᴛ 🌿</b>', show_alert=True)
         except UserIsBlocked:
-            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
+            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ !', show_alert=True)
         except PeerIdInvalid:
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
         except Exception as e:
